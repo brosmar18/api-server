cbcda74fd001fba47c16535d4c495c1939277931
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-09-30 19:51:32 -0500

    404 and 500 error handlers added

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const express = require('express');
+const notFound = require('./handlers/404');
+const errorHandler = require('./handlers/500');
 
 require('dotenv').config();
-
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -11,6 +12,9 @@ const app = express();
 app.get('/', (req, res, next) => {
     res.status(200).send('Hello World!');
 });
+
+app.use('*', notFound);
+app.use(errorHandler);
 
 const start = () => {
     app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
