5fae0460c29ada64c18b2ae8141aff547c925e68
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-10-01 09:41:42 -0500

    food get route added

--- src/routes/food.js
+++ src/routes/food.js
@@ -5,6 +5,11 @@ const { foodModel } = require('../models');
 
 const router = express.Router();
 
+router.get('/food', async (req, res, next) => {
+    const food = await foodModel.findAll();
+    res.status(200).send(food);
+});
+
 router.post('/food', async (req, res, next) => {
     try {
         const newFood = await foodModel.create(req.body);
